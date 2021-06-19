c5f8781c2e256288c2981002e1008b1c91a88c4b
Ahmed Mahmoud <a7med.mahmoud2004@gmail.com>
2021-06-19 13:24:08 +0200

    feat: run the app

--- main.go
+++ main.go
@@ -1,5 +1,43 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func getInput(prompt string, r *bufio.Reader) (string, error) {
+	fmt.Print(prompt)
+	input, err := r.ReadString('\n')
+
+	return strings.TrimSpace(input), err
+}
+
+func run(t string) {
+	lexer := NewLexer(t)
+	tokens, err := lexer.MakeTokens()
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	for _, v := range tokens {
+		fmt.Println(v.ToString())
+	}
+}
+
 func main() {
-	println("Here we go!")
+	reader := bufio.NewReader(os.Stdin)
+
+	for {
+		text, err := getInput("\033[33mluminary %\033[37m ", reader)
+		if err != nil {
+			panic(err)
+		}
+		if (text == "exit") {
+			break
+		}
+		run(text)
+	}
 }
